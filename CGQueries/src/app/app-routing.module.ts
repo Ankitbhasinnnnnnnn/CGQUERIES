f4891c6b3dfe020ced508306c46dba727d5aaef4
Vanshika <89936851+Vanshika0421@users.noreply.github.com>
2023-04-18 19:45:30 +0530

    Login and verify

--- CGQueries/src/app/app-routing.module.ts
+++ CGQueries/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './Components/login/login.component';
+import { VerifyComponent } from './Components/verify/verify.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: 'login', component: LoginComponent},
+{path: 'verify', component: VerifyComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- CGQueries/src/app/app.module.ts
+++ CGQueries/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './Components/login/login.component';
+import { VerifyComponent } from './Components/verify/verify.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    VerifyComponent,
+
   ],
   imports: [
     BrowserModule,
